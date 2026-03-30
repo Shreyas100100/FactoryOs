e8a8b62e26546bfc2dafd89a8dbb0c9e49830734
Shreyas100100 <shreyaskulkarni1729@gmail.com>
2026-03-31 00:28:11 +0530

    setup

---
+++ src/test/java/com/FactoryOS/FactoryOsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FactoryOS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FactoryOsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
